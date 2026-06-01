a464cf085bc41a346e2b443f3b7da147b3c57078
DanielTonioti <daniel_tonioti@estudante.sesisenai.org.br>
2026-06-01 12:44:33 -0300

    doc - documentado o home

--- public/home.php
+++ public/home.php
@@ -4,18 +4,21 @@ if(!isset($_SESSION["usuario"])){
     header("Location: ../index.php");
     exit();
 }
+// nao permite a entrada se não tiver logado
 
 include("../infra/db/connect.php");
+// vai chamar o connect para essa página
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     $novoUsuario = $_POST['usuario'];
     $novaSenha = $_POST['senha'];
-
+    // variaveis que vão definir o novo usuario
     $sql = "INSERT INTO usuarios (usuario,senha) 
-    VALUES ('$novoUsuario','$novaSenha')";  
+    VALUES ('$novoUsuario','$novaSenha')"; 
+    // insere novos valores para o db
 
     if($conn->query($sql) === TRUE){
-        //query sql vai pesquisar 
+        //query sql vai pesquisar no db
         echo "<script> alert('Usuário cadastrado com sucesso!')</script>";
     }else{
         echo "<script> alert('Erro ao cadastrar')</script>";
@@ -33,6 +36,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 </head>
 <body>
     <h3>Bem-Vindo! <?php echo $_SESSION["usuario"]; ?></h3>
+    <!-- Mostra a mensagem de boas vindas -->
     <a href="logout.php"> Sair</a>
 
     <hr>
@@ -48,6 +52,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         
             if(isset($erro)){
                 echo $erro;
+            // exibe a mensagem de erro
             };
         
         ?>
@@ -58,6 +63,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <?php
     
     include("components/table.php")
+    // chama o documento de table que mostra os usuarios
 
     ?>
 
