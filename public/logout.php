18612aedaccfa595005b9329ca1632978cb098a9
DanielTonioti <daniel_tonioti@estudante.sesisenai.org.br>
2026-06-01 12:45:56 -0300

    doc - documentado a página logout

--- public/logout.php
+++ public/logout.php
@@ -1,8 +1,11 @@
 <?php
 
     session_start();
+    // incia a sessão
     session_destroy();
+    // Destroi a seção
     header("Location: ../index.php");
+    // Envia para o index
     exit();
 
 ?>
