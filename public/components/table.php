894390138e4f9f1e94c8b01a6ff6d72d7289b918
DanielTonioti <daniel_tonioti@estudante.sesisenai.org.br>
2026-06-08 11:32:59 -0300

    add - funções adicionadas conforme passado a aula

--- public/components/table.php
+++ public/components/table.php
@@ -6,21 +6,27 @@
         <th>ID</th>
         <th>Usuário</th>
         <th>Senha</th>
+        <th>Excluir</th>
+        <th>Editar</th>
     </tr>
 
     <?php
     
     $sqlTodosUsuarios = "SELECT * FROM usuarios";
-    // seleciona todos da tabela usuarios
+
     $resultadoTodosUsuarios = $conn->query($sqlTodosUsuarios);
-    // pega o resultado ddo select all from usuarios
+
     while($linha = $resultadoTodosUsuarios->fetch_assoc()){
-    // o fetch assoc associa pega as info e separa pelas colunas
+
+    // o fetch assoc
 
         echo "  <tr>
                     <td>". $linha['id'] . "</td>
                     <td>". $linha['usuario'] . "</td>
                     <td>". $linha['senha'] . "</td>
+                    <td> <a href='excluir.php?id=". $linha['id'] ."'> Excluir</td>
+
+                    <td> <a href='editar.php?id=". $linha['id'] ."'> Editar</td>
                 </tr>
         ";
 
--- public/home.php
+++ public/home.php
@@ -7,87 +7,60 @@ if(!isset($_SESSION["usuario"])){
 
 include("../infra/db/connect.php");
 
-$mensagem = ""; // Variável para concentrar os alertas
-
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-if(isset($_POST['btn-cadastrar'])){
     $novoUsuario = $_POST['usuario'];
     $novaSenha = $_POST['senha'];
-    $sql = "INSERT INTO usuarios (usuario,senha) 
-    VALUES ('$novoUsuario','$novaSenha')"; 
-    $conn->query($sql);
-}
 
-if(isset($_POST['btn-atualizar'])){
-    $atualizacaoId = $_POST['id-atualizacao'];
-    $atualizacaoUsuario = $_POST['usuario-atualizacao'];
-    $atualizacaoSenha = $_POST['senha-atualizacao'];
-    
-    $sql = "UPDATE usuarios 
-    SET usuario = '$atualizacaoUsuario', senha = '$atualizacaoSenha' 
-    WHERE id = $atualizacaoId";
-    $conn->query($sql);
-}
-if(isset($_POST['btn-deletar'])){
-    $deleteId = $_POST['id-delete'];
-    $sql = "DELETE FROM usuarios 
-    WHERE id = $deleteId";
-    $conn->query($sql);
-}
-        }
+    $sql = "INSERT INTO usuarios (usuario,senha) 
+    VALUES ('$novoUsuario','$novaSenha')";  
+
+    if($conn->query($sql) === TRUE){
+        echo "<script> alert('Usuário cadastrado com sucesso!')</script>";
+    }else{
+        echo "<script> alert('Erro ao cadastrar')</script>";
+    }
+
+};
+
 ?>
 
-<html lang="pt-br">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
 </head>
 <body>
-        <h3>Bem-Vindo! <?php echo $_SESSION["usuario"]; ?></h3>
-    <a href="logout.php">Sair</a>
+    <h3>Bem-Vindo! <?php echo $_SESSION["usuario"]; ?></h3>
+    <a href="logout.php"> Sair</a>
 
     <hr>
-    
-    <h4>Cadastro de Novo Usuário</h4>
+    <h4>Cadastro de Novo Usuário.</h4>
     <form method="POST">
         <label>Usuário:</label>
-        <input type="text" name="usuario" required>
-        <br><br>
+        <input type="text" name="usuario">
+        <br>
         <label>Senha:</label>
-        <input type="password" name="senha" required>
-        <br><br>
-        <button type="submit" name="btn-cadastrar">Cadastrar</button>
-    </form>
-    
-    <hr>
-    
-    <h4>Atualização de Usuário</h4>
-    <form method="POST">
-        <label>ID:</label>
-        <input type="number" name="id-atualizacao" required>
-        <br><br>
-        <label>Novo Usuário:</label>
-        <input type="text" name="usuario-atualizacao" required>
-        <br><br>
-        <label>Nova Senha:</label>
-        <input type="password" name="senha-atualizacao" required>
-        <br><br>
-        <button type="submit" name="btn-atualizar">Atualizar</button>
+        <input type="password" name="senha">
+        <br>
+        <?php
+        
+            if(isset($erro)){
+                echo $erro;
+            };
+        
+        ?>
+        <br>
+        <button type="submit">Cadastrar</button>
     </form>
     <hr>
-    <h4>Deletar usuario</h4>
-    <form method="POST">
-        <label>ID:</label>
-        <input type="number" name="id-delete" required><br><br>
-        <button type="submit" name="btn-deletar">Deletar</button>
-    </form>
+    <?php
     
-    <hr>
+    include("components/table.php")
 
-    <?php 
-    // Inclui a tabela que lista os usuários
-    include("components/table.php"); 
     ?>
+
+
+
 </body>
 </html>
